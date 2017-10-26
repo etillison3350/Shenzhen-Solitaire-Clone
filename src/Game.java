0a9b9d44459a59f38de35760174f1f433125c9df
ricky3350 <ricky3350@users.noreply.github.com>
2017-10-25 23:23:32 -0400

    All conditions for move method

--- src/Game.java
+++ src/Game.java
@@ -34,7 +34,12 @@ public class Game {
 			board.add(cards.stream().skip(i * 5).limit(5).collect(Collectors.toCollection(ArrayList::new)));
 		}
 
-		Collections.fill(topBoard, -1);
+		for (int i = 0; i < 3; i++) {
+			topBoard.add(-1);
+		}
+		for (int i = 0; i < 4; i++) {
+			topBoard.add(0);
+		}
 	}
 
 	public int cardsIn(final int slot) {
@@ -54,7 +59,7 @@ public class Game {
 	public int highestComplete(final int color) {
 		if (color < 0) throw new IndexOutOfBoundsException();
 
-		return topBoard.get(color + 4);
+		return topBoard.get(color + 4) & 0b1111;
 	}
 
 	public boolean rose() {
@@ -70,23 +75,31 @@ public class Game {
 	 * @param srcSlot
 	 * @param srcIndex - <i>Note: a value of -2 will always return <code>false</code></i>
 	 * @param destSlot
-	 * @param destIndex - <i>Note: all positive values of destIndex have the same function</i>
+	 * @param destIndex - <i>Note: all positive values of destIndex have the same function. Also,
+	 *            moving a dragon to an index of -2 attempts to collect all dragons to the sideboard
+	 *            slots.</i>
 	 * @return whether or not the movement was successful (i.e. if it was allowed)
 	 */
 	public boolean move(final int srcSlot, final int srcIndex, final int destSlot, final int destIndex) {
 		if (srcSlot > 7 || srcSlot < 0) throw new IllegalArgumentException("Source slot (" + srcSlot + ") out of range");
 		if (destSlot > 7 || destSlot < 0) throw new IllegalArgumentException("Destination slot (" + destSlot + ") out of range");
 
+		if (srcIndex < -2) throw new IllegalArgumentException("Source index (" + srcIndex + ") out of range");
+		if (destIndex < -2) throw new IllegalArgumentException("Destination index (" + destIndex + ") out of range");
+
 		int numCards = 1; // The number of cards to be moved.
+		int bottomCard = -1;
 
 		if (srcIndex == -2) {
 			return false; // Cannot move card out of completed zone
 		} else if (srcIndex == -1) {
-			if (sideboardCard(srcSlot) < 0) { // Attempting to move no card (empty space)
+			bottomCard = sideboardCard(srcSlot);
+			if (bottomCard < 0) { // Attempting to move no card (empty space)
 				return false;
 			}
+
 		} else {
-			int value = cardAt(srcSlot, srcIndex);
+			int value = bottomCard = cardAt(srcSlot, srcIndex);
 			for (int i = srcIndex + 1; i < cardsIn(srcSlot); i++) {
 				final int n = cardAt(srcSlot, i);
 
@@ -102,6 +115,58 @@ public class Game {
 
 		// Cannot move more than one card to the top bar
 		if (destIndex < 0 && numCards > 1) return false;
+
+		if (destIndex == -2) {
+			// moving a dragon to an index of -2 attempts to collect all dragons to the sideboard
+			// slots.
+			if ((bottomCard & 0b1001111) == DRAGON_MOD) {
+
+				// No slots available
+				if (!topBoard.stream().limit(3).anyMatch(i -> i == 0)) return false;
+
+				int ct = 0;
+				for (int i = 0; i < board.size(); i++) {
+					if (cardAt(i, cardsIn(i) - 1) == bottomCard) {
+						if (++ct >= 4) break;
+					}
+				}
+
+				if (ct != 4) {
+					for (int i = 0; i < 3; i++) {
+						if (sideboardCard(i) == bottomCard) {
+							if (++ct >= 4) break;
+						}
+					}
+
+					if (ct != 4) return false;
+				}
+			} else if (bottomCard != ROSE) { // The rose can always be completed if it has not yet
+												// been
+				final int color = bottomCard >> 4 & 0b11;
+
+				// Completed cards must be one higher than the current complete card.
+				if (highestComplete(color) != (bottomCard & 0b1111) - 1) return false;
+			}
+		} else if (destIndex == -1) {
+			if (sideboardCard(destSlot) >= 0) return false;
+		} else {
+			if (cardsIn(destSlot) != 0) {
+				final int below = cardAt(destSlot, cardsIn(destSlot) - 1);
+
+				// Cannot place dragons on any card
+				if ((bottomCard & 0b1001111) == DRAGON_MOD) return false;
+
+				// Cannot place if numbers do not match
+				if ((below & 0b1111) - 1 != (bottomCard & 0b1111)) return false;
+
+				// Cannot place matching colors on top of each other.
+				if ((below & 0b110000) == (bottomCard & 0b110000)) return false;
+			}
+		}
+
+		// TODO perform movement
+
+		return true;
 	}
 
 }
